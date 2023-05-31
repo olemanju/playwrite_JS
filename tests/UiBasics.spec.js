8ed30a15b78364e7df0c3d9f9b272010224b253b
olemanju <manju.ole23@gmail.com>
2023-05-31 23:37:06 +1000

    updated files

--- tests/UiBasics.spec.js
+++ tests/UiBasics.spec.js
@@ -18,7 +18,7 @@ test('first test case', async ({browser})=>{
 }) 
 
 //this methods will work without context details
-test.only('Second test case', async ({page})=>{
+test.only('ThSecondird test case', async ({page})=>{
 
     var url ="https://rahulshettyacademy.com/loginpagePractise/";
     //To launchthe URL
@@ -42,5 +42,60 @@ test.only('Second test case', async ({page})=>{
     //Assertions we have used
     await expect(await page.locator("[style*='block']")).toContainText("Incorrect")
 
+    //another method for entering the text
+    await page.locator('#username').fill("")
+
+    await page.locator('#username').fill("rahulshettyacademy")
+    await page.locator("#signInBtn").click()
+
+    //Next page
+
+    const productsnames =  page.locator(".card-body a")
+
+    //To print the first value
+    console.log(await productsnames.first().textContent())
+    //To print the second value
+    console.log(await productsnames.nth(2).textContent())
+    //to print last
+    console.log(await productsnames.last().textContent())
+
+    })      
+
+    //this methods will work without context details
+test.only('Third test case to print all the links', async ({page})=>{
+
+    var url ="https://rahulshettyacademy.com/loginpagePractise/";
+    //To launchthe URL
+    await page.goto(url)
+
+    var titlepage = "LoginPage Practise | Rahul Shetty Academy"
+    //User assertions to verify the title
+    await expect(page).toHaveTitle(titlepage)
     
+    //page.locator we use instead of $ or driver.findelementby
+    //type is used instead of sendkeys
+    await page.locator('#username').type("rahulshettyacademy1")
+    await page.locator('#password').type("learning")
+    await page.locator("#signInBtn").click()
+    //Actually it will take more time see this message
+    //but play wright is cleaver enough to handle this
+    //textContent is used for gettext()
+    const errormessage = await page.locator("[style*='block']").textContent()
+    console.log(errormessage)
+
+    //Assertions we have used
+    await expect(await page.locator("[style*='block']")).toContainText("Incorrect")
+
+    //another method for entering the text
+    await page.locator('#username').fill("")
+
+    await page.locator('#username').fill("rahulshettyacademy")
+    //await page.locator("#signInBtn").click()
+
+    //Next page
+
+    const productsnames =  page.locator(".card-body a")
+
+   
+
     })      
